59fb90e2542d37fcc38ce9e5420a15620f22f688
jimbo05gen08 <161784410+jimbo05gen08@users.noreply.github.com>
2024-07-03 23:21:21 -0500

    subiendo cambios de proyecto Olivia´s Bakery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  AngularEditorConfig,
+  AngularEditorModule,
+} from '@kolkov/angular-editor';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    AngularEditorModule,
+    FormsModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Practicas';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./modules/layout/layout.component'),
+        children: [
+          { path: '', redirectTo: 'home', pathMatch: 'full' },
+          {
+            path: 'home',
+            loadComponent: () => import('./modules/home/home.component'),
+          },
+          {
+            path: 'us',
+            loadComponent: () => import('./modules/us/us.component'),
+          },
+          {
+            path: 'signin',
+            loadComponent: () => import('./modules/signin/signin.component'),
+          },
+          {
+            path: 'register',
+            loadComponent: () =>
+              import('./modules/signin/register/register.component'),
+          },
+          {
+            path: 'catalogue',
+            loadComponent: () =>
+              import('./modules/catalogue/catalogue.component'),
+          },
+          {
+            path: 'contact',
+            loadComponent: () => import('./modules/contact/contact.component'),
+          },
+        ],
+      },
+    ],
+  },
+];
